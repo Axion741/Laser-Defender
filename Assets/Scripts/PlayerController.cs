de7959d4f3936f332633ee963b61bb97ff58febb
Unknown <k.ballagher@gmail.com>
2017-04-21 13:45:10 +0100

    Clamped Player Sprite within Camera bounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,25 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 12.0f;
-
-
-
+    float padding;
+    float xmin;
+    float xmax;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        padding = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.extents.x;
+        xmin = leftmost.x + padding;
+        xmax = rightmost.x - padding;
+       
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.position += new Vector3(+speed * Time.deltaTime, 0, 0);
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
+        //restrict player to gamespace
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+
     }
 
-  
-   
+
 }
