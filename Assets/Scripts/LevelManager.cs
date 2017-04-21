f4825e9279304049d2de5a391c15883fc4608082
Unknown <k.ballagher@gmail.com>
2017-04-21 13:07:04 +0100

    Imported Menu System. Added Player Sprite + Controller
    Imported menu system based on Block Breaker.
    Imported Player ship sprite + created PlayerController Script.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level Load Requested for: " + name);
+        SceneManager.LoadScene(name);
+       
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit Request logged");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+       
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 12.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += new Vector3(+speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+  
+   
+}
